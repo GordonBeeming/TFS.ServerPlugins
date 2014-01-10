a339eaa8ad1c12ce2088725b81b830e0554982a9
Gordon-Beeming <gordon@beeming.co.za>
2014-01-10 10:09:36 +0200

    Added TFS.EnforceCheckInPolicies Subscriber

---
+++ TFS.EnforceCheckInPolicies/TFS.EnforceCheckInPolicies/EnforceCheckInPoliciesSubscriber.cs
@@ -0,0 +1,65 @@
+﻿namespace TFS.EnforceCheckInPolicies
+{
+    using System;
+    using System.Diagnostics;
+
+    using Microsoft.TeamFoundation.Common;
+    using Microsoft.TeamFoundation.Framework.Server;
+    using Microsoft.TeamFoundation.VersionControl.Server;
+
+    public class EnforceCheckInPoliciesSubscriber : ISubscriber
+    {
+        public string Name
+        {
+            get
+            {
+                return "TFS.EnforceCheckInPolicies";
+            }
+        }
+
+        public SubscriberPriority Priority
+        {
+            get
+            {
+                return SubscriberPriority.High;
+            }
+        }
+
+        public EventNotificationStatus ProcessEvent(TeamFoundationRequestContext requestContext, NotificationType notificationType, object notificationEventArgs, out int statusCode, out string statusMessage, out ExceptionPropertyCollection properties)
+        {
+            statusCode = 0;
+            properties = null;
+            statusMessage = string.Empty;
+
+            try
+            {
+                if (notificationType == NotificationType.DecisionPoint &&
+                    notificationEventArgs is CheckinNotification)
+                {
+                    var args = notificationEventArgs as
+                               CheckinNotification;
+                    if (args.PolicyOverrideInfo.PolicyFailures.Length > 0)
+                    {
+                        statusMessage = "Policy Overriding is not allowed.";
+                        return EventNotificationStatus.ActionDenied;
+                    }
+                }
+                return EventNotificationStatus.ActionPermitted;
+            }
+            catch (Exception ex)
+            {
+                // log the error and fail the check in
+                statusMessage = "Error in plugin '" + this.Name + "', error details: "
+                                + ex;
+                EventLog.WriteEntry("TFS Service", statusMessage,
+                    EventLogEntryType.Error);
+                return EventNotificationStatus.ActionDenied;
+            }
+        }
+
+        public Type[] SubscribedTypes()
+        {
+            return new[] { typeof(CheckinNotification) };
+        }
+    }
+}
